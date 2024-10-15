ac83c6c0479609dcad9992cb8132f97c32a52fe5
Ilya Zvezdin <ilya.zvezdin@gmail.com>
2024-10-15 15:26:04 +0500

    homework05: применил smart casting для горловины при заправке

--- src/main/kotlin/ru/otus/cars/GasStation.kt
+++ src/main/kotlin/ru/otus/cars/GasStation.kt
@@ -4,9 +4,9 @@ package ru.otus.cars
 fun fillCar(car: Car, liters: Int) {
     try {
         car.tankMouth.open()
-        when (car.tankMouth) {
-            is TankMouth.PETROL -> (car.tankMouth as TankMouth.PETROL).fuelPetrol(liters)
-            is TankMouth.LPG -> (car.tankMouth as TankMouth.LPG).fuelLPG(liters)
+        when (val mouth = car.tankMouth) {
+            is TankMouth.PETROL -> mouth.fuelPetrol(liters)
+            is TankMouth.LPG -> mouth.fuelLPG(liters)
         }
         car.tankMouth.close()
     } catch (e: Exception) {
